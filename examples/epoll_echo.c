d9840c7f77a6e3bdebac6c17720c84a4560e7ff2
pylover <vahid.mardani@gmail.com>
2025-09-16 19:57:25 +0330

    Feature: console echo example using epoll module

---
+++ examples/epoll_echo.c
@@ -0,0 +1,78 @@
+// Copyright 2025 Vahid Mardani
+/*
+ * This file is part of pcaio.
+ *  pcaio is free software: you can redistribute it and/or modify it under
+ *  the terms of the GNU General Public License as published by the Free
+ *  Software Foundation, either version 3 of the License, or (at your option)
+ *  any later version.
+ *
+ *  pcaio is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *  FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ *  details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with pcaio. If not, see <https://www.gnu.org/licenses/>.
+ *
+ *  Author: Vahid Mardani <vahid.mardani@gmail.com>
+ */
+/* standard */
+#include <stdio.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/* thirdparty */
+#include <clog.h>
+
+/* pcaio */
+#include <pcaio/pcaio.h>
+#include <pcaio/modio.h>
+#include <pcaio/modepoll.h>
+
+
+#define CHUNKSIZE 80
+#define WORKERS 1
+
+
+static int
+_echo(int argc, void *argv[]) {
+    int ret = 0;
+    char buff[CHUNKSIZE];
+
+    printf("write something then press enter.\n");
+    for (;;) {
+        ret = await_read(STDIN_FILENO, buff, CHUNKSIZE);
+        if (ret <= 0) {
+            break;
+        }
+
+        printf("%.*s", ret, buff);
+    }
+
+    return ret;
+}
+
+
+int
+main() {
+    int ret;
+    struct pcaio_iomodule *modepoll;
+    struct pcaio_task *t;
+
+    /* make standard input nonblock */
+    fcntl(STDIN_FILENO, F_SETFL, fcntl(STDIN_FILENO, F_GETFL) | O_NONBLOCK);
+
+    /* create a task */
+    t = pcaio_task_new(_echo, 0);
+
+    /* create and register the epoll module */
+    pcaio_modepoll_use(4, &modepoll);
+    pcaio_modio_use(modepoll);
+
+    /* main loop */
+    ret = pcaio(WORKERS, &t, 1);
+
+    return ret;
+}
